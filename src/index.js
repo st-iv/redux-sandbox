ffc1855679cb021d6c3569124b13750e5da05017
stiv <stepan.ivanov93@gmail.com>
2019-06-08 02:09:40 +0800

    #add inc action

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('redux-sanbox');
+const reducer = (state = 0, action) => {
+
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+        default:
+            return state;
+    }
+};
+
+let state = reducer(0, {type: 'INC'});
+
+console.log(state);
